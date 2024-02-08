4f1a7508e19158c1742891a39b5c70ba1f29e32e
Carlos Alessandro <carllosalessandro@gmail.com>
2024-02-07 21:43:17 -0300

    feat: add types adn depedencies

---
+++ types/threads.ts
@@ -0,0 +1,29 @@
+export interface Threads {
+  id: string;
+  author: User;
+  content: string;
+  image?: string;
+  replies?: Reply[];
+  repliesCount: number;
+  likesCount: number;
+  mention?: boolean;
+  mentionUser?: User;
+  createdAt?: string;
+}
+export interface User {
+  id: string;
+  name: string;
+  userName: string;
+  verified: boolean;
+  photo: string;
+  bio: string;
+  link?: string;
+  followers?: User[];
+}
+export interface Reply {
+  id: string;
+  author: string;
+  content: string;
+  likes: number;
+  createdAt: string;
+}
